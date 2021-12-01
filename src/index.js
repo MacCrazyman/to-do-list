4be767a8b83b5bcf7056aca7d407a42883dc7b7b
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-01 14:41:15 -0500

    Add enter icon

--- src/index.js
+++ src/index.js
@@ -28,14 +28,23 @@ function create_task(task){
   const list_item = document.createElement('li')
   // list_item.classList.add('flex')
   list_item.innerHTML =`<div class="flex cell">
-          <input type="checkbox">
+          <input class="checkbox" type="checkbox">
           <textarea class="cell_textarea" rows="1">${task.description}</textarea>
           <div class="drag_icon"></div>
           </div>`;
+  
   list_container.appendChild(list_item);
+  
 }
-
-// Event listeners
 
 // Call functions
 list.forEach((task) => create_task(task))
+
+// Event listeners
+  const check = document.querySelectorAll('.checkbox');
+  const text = document.querySelectorAll('.cell_textarea')
+  check.forEach((element) =>  {element.addEventListener('click',() => {
+    element.classList.toggle('check');
+    element.nextElementSibling.classList.toggle('mark');
+  });}
+  )
