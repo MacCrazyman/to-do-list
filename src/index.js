6e92ddce3c69aea420eb33c79bd486511238bf94
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-01 09:57:58 -0500

    Create task dinamically

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
+// Imports
 import './style.css'
 
+// variables
 const list = [
   {
     description : 'some description',
     completed : 0,
     index : 0
   },
+  {
+    description : 'today\'s task',
+    completed : 0,
+    index : 0
+  },
+  {
+    description : 'yesterday\'s tast description',
+    completed : 0,
+    index : 0
+  },
 ];
+
+// Query selectors
+const list_container = document.querySelector('#list_container');
+
+// Functions
+function create_task(task){
+  const list_item = document.createElement('li')
+  list_item.classList.add('flex')
+  list_item.innerHTML =`
+          <input type="checkbox">
+          <textarea>${task.description}</textarea>
+          <div class="drag_icon"></div>
+        `;
+  list_container.appendChild(list_item);
+}
+
+// Event listeners
+
+// Call functions
+list.forEach((task) => create_task(task))
