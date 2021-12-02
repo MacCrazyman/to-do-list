58f17521c94fbe2db5ddd5f6f4478c784ac30249
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-02 13:31:39 -0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -9,41 +9,6 @@ const list = new Status();
 const listContainer = document.querySelector('#list_container');
 
 // Functions
-function createTask(task) {
-  const listItem = document.createElement('li');
-  const divItem = document.createElement('div');
-  const taskcheck = document.createElement('input');
-  const taskText = document.createElement('textarea');
-  const dragIcon = document.createElement('div');
-
-  divItem.id = task.index;
-  divItem.classList.add('flex','cell');
-  taskcheck.setAttribute('type','checkbox');
-  taskcheck.classList.add('checkbox');
-  taskcheck.checked=task.completed;
-
-  taskText.classList.add('cell_textarea');
-  taskText.setAttribute('rows','1')
-  taskText.innerHTML=task.description;
-
-  dragIcon.classList.add('drag_icon')
-  
-  if (task.completed) {
-    taskcheck.classList.add('check');
-    taskText.classList.add('mark');
-  }
-
-  listItem.appendChild(divItem);
-  divItem.appendChild(taskcheck);
-  divItem.appendChild(taskText);
-  divItem.appendChild(dragIcon);
-  listContainer.appendChild(listItem);
-
-  taskcheck.addEventListener('click', () => {
-    taskcheck.checked ? markDone(taskcheck) : unmarkDone(taskcheck);
-  });
-}
-
 function markDone(element) {
   element.classList.add('check');
   list.mark(element.parentElement.id);
@@ -56,9 +21,42 @@ function unmarkDone(element) {
   element.nextElementSibling.classList.remove('mark');
 }
 
+function createTask(task) {
+  const listItem = document.createElement('li');
+  const divItem = document.createElement('div');
+  const taskcheck = document.createElement('input');
+  const taskText = document.createElement('textarea');
+  const dragIcon = document.createElement('div');
+
+  divItem.id = task.index;
+  divItem.classList.add('flex', 'cell');
+  taskcheck.setAttribute('type', 'checkbox');
+  taskcheck.classList.add('checkbox');
+  taskcheck.checked = task.completed;
+
+  taskText.classList.add('cell_textarea');
+  taskText.setAttribute('rows', '1');
+  taskText.innerHTML = task.description;
+
+  dragIcon.classList.add('drag_icon');
+
+  if (task.completed) {
+    taskcheck.classList.add('check');
+    taskText.classList.add('mark');
+  }
+
+  listItem.appendChild(divItem);
+  divItem.appendChild(taskcheck);
+  divItem.appendChild(taskText);
+  divItem.appendChild(dragIcon);
+  listContainer.appendChild(listItem);
+
+  taskcheck.addEventListener('click', () => (taskcheck.checked ? markDone(taskcheck) : unmarkDone(taskcheck)));
+}
+
 // Call functions
 
 // Event listeners
 window.addEventListener('DOMContentLoaded', () => {
   Object.values(list.list).forEach((value) => createTask(value));
-})
+});
