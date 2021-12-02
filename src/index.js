f8b0c1472aea2496eb900fe4ed561b9797801011
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-02 10:41:14 -0500

    Import file with methods to change state

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 // Imports
 import './style.css';
+import { status } from './status';
 
 // variables
-const list = [
-  {
-    description: 'some description',
-    completed: 0,
-    index: 0,
-  },
-  {
-    description: 'today\'s task',
-    completed: 0,
-    index: 0,
-  },
-  {
-    description: 'yesterday\'s tast description',
-    completed: 0,
-    index: 0,
-  },
-];
+const list = new status();
+// const list = [
+//   {
+//     description: 'some description',
+//     completed: 0,
+//     index: 0,
+//   },
+//   {
+//     description: 'today\'s task',
+//     completed: 0,
+//     index: 0,
+//   },
+//   {
+//     description: 'yesterday\'s tast description',
+//     completed: 0,
+//     index: 0,
+//   },
+// ];
 
 // Query selectors
 const listContainer = document.querySelector('#list_container');
@@ -27,7 +29,7 @@ const listContainer = document.querySelector('#list_container');
 function createTask(task) {
   const listItem = document.createElement('li');
   // listItem.classList.add('flex')
-  listItem.innerHTML = `<div class="flex cell">
+  listItem.innerHTML = `<div id="${task.index}" class="flex cell">
           <input class="checkbox" type="checkbox">
           <textarea class="cell_textarea" rows="1">${task.description}</textarea>
           <div class="drag_icon"></div>
@@ -37,13 +39,14 @@ function createTask(task) {
 }
 
 // Call functions
-list.forEach((task) => createTask(task));
+for (const task in list.list) createTask(list.list[task]);
 
 // Event listeners
 const check = document.querySelectorAll('.checkbox');
 check.forEach((element) => {
   element.addEventListener('click', () => {
     element.classList.toggle('check');
+    list.change(element.parentElement.id);
     element.nextElementSibling.classList.toggle('mark');
   });
 });
---
+++ src/status.js
@@ -0,0 +1,37 @@
+export class status {
+  constructor() {
+    if (localStorage.getItem('list') === null) {
+    this.list = {
+      0: {
+        description: 'some description',
+        completed: false,
+        index: 0,
+      },
+      1: {
+        description: 'today\'s task',
+        completed: false,
+        index: 1,
+      },
+      2: {
+        description: 'yesterday\'s tast description',
+        completed: false,
+        index: 2,
+      },
+    };
+    } else {
+      this.list = JSON.parse(localStorage.getItem('list'));
+    }
+
+    
+  }
+
+  saveStorage = () => {
+    localStorage.setItem('list', JSON.stringify(this.list))
+  }
+  
+  change = (index) => {
+    const task = this.list[index];
+    task.completed = !task.completed;
+    this.saveStorage();
+  }
+}
