74ce3d1e3b26b2e407ea098dc2341e642a95503f
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-09 14:23:00 -0500

    Set up jest

--- test-files/task.test.js
+++ test-files/task.test.js
@@ -1,5 +1,7 @@
-import * as task from '../src/task';
-jest.mock('../src/__mocks__')
+const task = require('../src/task')
+// import * as task from '../src/task';
+// jest.mock('../src/__mocks__')
+console.log(task);
 describe('Testing add and remove', () => {
      
 })
