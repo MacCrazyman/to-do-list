a7e6d0278fbdab8b3716e962ffbff2e14f9cde0d
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-12-09 22:37:51 +0200

    Add remove function test

--- test-files/task.test.js
+++ test-files/task.test.js
@@ -1,23 +1,42 @@
-const task = require('../src/task')
-  const newTask ={
-      value: 'first task',
-    };
-  const list={
-    list: [
-      {
-        description: 'first task',
-        completed: false,
-        index:0
-      },
-    ]
-  };
+import { JSDOM } from 'jsdom';
 
+const task = require('../src/task');
 
+const newTask = {
+  value: 'first task',
+};
+const list = {
+  list: [
+    {
+      description: 'first task',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'second task',
+      completed: false,
+      index: 1,
+    },
+  ],
+};
 
 describe('Testing add and remove', () => {
+  test('Test add function', () => {
+    task.add(newTask, list);
+    expect(list.list).toHaveLength(3);
+  });
 
   test('Test add function', () => {
-    task.add(newTask,list);
-    expect(list.list).toHaveLength(2);
-  })
-})
+    document.body.innerHTML = `<ul id="list_container">
+  <li><div class="flex cell"><input type="checkbox" class="checkbox"><input class="cell_textarea" id ="1" type="text"><span class="drag_icon">⋮</span></div></li>
+  <li><div class="flex cell"><input type="checkbox" class="checkbox"><input class="cell_textarea" id ="2" type="text"><span class="drag_icon">⋮</span></div></li>
+  </ul>`;
+
+    const task1 = document.getElementById('1');
+    const task2 = document.getElementById('2');
+
+    task.remove(list, task1, list.list[0]);
+    expect(list.list).toHaveLength(1);
+    
+  });
+});
