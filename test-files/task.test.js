ed3292eca271eb27599b254e41623c84fa01a3f9
vicmaburrito <vmaldaraca@gmail.com>
2021-12-09 14:58:26 -0600

    Add babel to use Jsdom to create elements to test in jest

--- test-files/task.test.js
+++ test-files/task.test.js
@@ -1,5 +1,5 @@
 import { JSDOM } from 'jsdom';
-
+const dom = new JSDOM(); global.document = dom.window.document; global.window = dom.window;
 const task = require('../src/task');
 
 const newTask = {
