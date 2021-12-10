3d77fef82930c5d17fe6de8cbe87d778fb3a9769
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-12-10 13:05:51 -0500

    Add test to check if elements are cleared

--- test-files/index.test.js
+++ test-files/index.test.js
@@ -1,18 +1,24 @@
-import { JSDOM } from 'jsdom';
 import * as task from '../src/task.js';
 import Status from '../src/status.js';
 
 const list = new Status();
-const dom = new JSDOM();
-global.document = dom.window.document;
-global.window = dom.window;
-
 
 describe('testing edit function', () => {
-  const taskElement = { description: 'task Element Object!', completed: false, index: 0 };
+  list.list[0] = { description: 'task Element Object!', completed: false, index: 0 };
+  const taskElement = list.list[0];
   const taskText = { value: 'New task Element Object!' };
   test('should change value of description', () => {
     task.updateTask(taskElement, taskText);
     expect(taskElement.description).toEqual(taskText.value);
   });
+
+  test('The status of the task should change from false to true', () =>{
+    list.mark(0);
+    expect(list.list[0].completed).toBeTruthy();
+  });
+
+  test('The array should be empty', () => {
+    task.clear(list);
+    expect(list.list).toHaveLength(0);
+  })
 });
