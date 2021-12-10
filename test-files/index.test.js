faa95971635bb508371a3cde17a6ce25db0fef3b
vicmaburrito <vmaldaraca@gmail.com>
2021-12-10 10:37:08 -0600

    Create updateTask test

--- test-files/index.test.js
+++ test-files/index.test.js
@@ -1,35 +1,17 @@
 import { JSDOM } from 'jsdom';
-import {markDone, unmarkDone, createTask} from '../src/index.js';
+import * as task from '../src/task.js';
 
+// jest.mock('./__mocks__/localStorage.js');
 
 const dom = new JSDOM();
 global.document = dom.window.document;
 global.window = dom.window;
-const task = require('../src/task.js');
-
-const newTask = {
-  value: 'first task',
-};
-const list = {
-  list: [
-    {
-      description: 'first task',
-      completed: false,
-      index: 0,
-    },
-    {
-      description: 'second task',
-      completed: false,
-      index: 1,
-    },
-  ],
-};
-
-const localStorage = {};
-
-document.body.innerHTML = `<ul id="list_container"></ul>`;
 
 describe('testing edit function', () => {
-  index.createTask(list.list[0]);
-  console.log(document.body.innerHTML);
+  const taskElement = { description: 'task Element Object!', completed: false, index: 0 };
+  const taskText = { value: 'New task Element Object!' };
+  test('should change value of description', () => {
+    task.updateTask(taskElement, taskText);
+    expect(taskElement.description).toEqual(taskText.value);
+  });
 });
